cad69e06325ac9d0cdace00f26e7ddf5c714efb1
de4thlol2d <apichet_masaya@hotmail.com>
2016-09-27 21:34:05 +0700

    รอดึงไฟล์

--- test.spec.js
+++ test.spec.js
@@ -13,9 +13,36 @@ function Tennis(pa,pb) {
   }
 
 describe("Tennis Score", function() {
-  it('should be "Love - Love" when score = 0 - 0', function() {
-    var str = Tennis(0,0);
-    expect(str).toEqual('Love-Love');
-  });
+  it('should be "Love - fifteen" when score = 0 - 15', function() {
+    var str = Tennis(0,15);
+    expect(str).toEqual('Love-fifteen');
+  })
+});
 
+describe("Tennis Score", function() {
+  it('should be "Love - thirty" when score = 0 - 30', function() {
+    var str = Tennis(0,30);
+    expect(str).toEqual('Love-thirty');
+  })
+});
+
+describe("Tennis Score", function() {
+  it('should be "Love - forty" when score = 0 - 40', function() {
+    var str = Tennis(0,40);
+    expect(str).toEqual('Love-forty');
+  })
+});
+
+describe("Tennis Score", function() {
+  it('should be "fifteen - fifteen" when score = 15 - 15', function() {
+     var str = Tennis(15,15);
+    expect(str).toEqual('fifteen-fifteen');
+   })
+});
+
+  describe("Tennis Score", function() {
+  it('should be "thirty - fifteen" when score = 30 - 15', function() {
+      var str = Tennis(30,15);
+      expect(str).toEqual('thirty-fifteen');
+    })
 });
