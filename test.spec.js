dddd00215441d6b4d4f2939f2a7805d585a736d8
highmember <ftn2539@gmail.com>
2016-09-28 15:13:54 +0700

    เพิ่มตัวนับคะแนน เก็บคะแนน

--- test.spec.js
+++ test.spec.js
@@ -1,26 +1,44 @@
-function Tennis(pa,pb) {
+function Tennis() {
+   spA = 0;
+   spB = 0;
+
+  this.Start = function(){
+    spA = 0; sp = 0 ;
+    return this.echo();
+  }
+
+  this.AGet = function(){
+    spA += 1 ;
+    return this.echo();
+  }
+
+  this.BGet = function(){
+    spB += 1 ;
+    return this.echo();
+  }
+
+ this.echo = function(){
   let voicea = '';
   let voiceb = '';
-
-  switch (pa) {
+  switch (spA) {
     case 0:voicea += 'Love';break;
     case 1:voicea += 'Fifteen';break;
     case 2:voicea += 'Thirty';break;
     case 3:voicea += 'Forty';break;
     case 4:voicea += 'Player A Won';break;
   }
-  switch (pb) {
+  switch (spB) {
     case 0:voiceb += 'Love';break;
     case 1:voiceb += 'Fifteen';break;
     case 2:voiceb += 'Thirty';break;
     case 3:voiceb += 'Forty';break;
     case 4:voiceb += 'Player B Won';break
   }
-  if(pa === 4)
+  if(spA === 4)
   {
     return voicea;
   }
-  else if(pb === 4)
+  else if(spB === 4)
   {
     return voiceb;
   }
@@ -29,24 +47,23 @@ function Tennis(pa,pb) {
     return voicea+'-'+voiceb ;
   }
 
-  }
-
-  describe("Tennis", function() {
-  it('should echo "Love - Fifteen" when score = 0 - 15', function() {
-    var str = Tennis(0,1);
-    expect(str).toEqual('Love-Fifteen');})
-  });
+}
+}
+var Play = new Tennis();
 
   describe("Tennis", function() {
   it('should echo "Love - Love" when score = 0 - 0', function() {
-    var str = Tennis(0,0);
-    expect(str).toEqual('Love-Love');})
+    expect(Play.Start()).toEqual('Love-Love');})
   });
 
   describe("Tennis", function() {
   it('should echo "Fifteen - Love" when score = 15 - 0', function() {
-    var str = Tennis(1,0);
-    expect(str).toEqual('Fifteen-Love');})
+    expect(Play.AGet()).toEqual('Fifteen-Love');})
+  });
+
+  /*describe("Tennis", function() {
+  it('should echo "Love - Fifteen" when score = 0 - 15', function() {
+    expect(Play.BGet()).toEqual('Love-Fifteen');})
   });
 
   describe("Tennis", function() {
@@ -95,4 +112,4 @@ function Tennis(pa,pb) {
   it('should echo "Thirty - Fifteen" when score = 30 - 15', function() {
     var str = Tennis(2,1);
     expect(str).toEqual('Thirty-Fifteen');})
-});
+});*/
